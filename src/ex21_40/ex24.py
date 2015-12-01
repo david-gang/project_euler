769252ed6bd9138b3c8764735a97a88c713360a2
David Michael Gang <michaelgang@gmail.com>
2015-12-01 10:07:25 +0200

    solved exercise 24

---
+++ src/ex21_40/ex24.py
@@ -0,0 +1,24 @@
+import time
+import math
+
+def lexographic_permutation(n,index):
+    index = index-1
+    initial_arr = list(range(n))
+    counter = math.factorial(n-1)
+    perms = []
+    for i in range(n-1,0,-1):
+        index, j = index%counter, index//counter
+        perms.append(initial_arr.pop(j))
+        counter = counter//i
+    perms.append(initial_arr[0])
+    return "".join(str(x) for x in perms)
+    
+res = lexographic_permutation(10,10**6)
+
+start = time.clock()
+print(res)
+end = time.clock()
+print("It took ",end - start," seconds")
+
+          
+    
