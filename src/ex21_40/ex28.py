ed87539a48971a8e803a12c8bcc17ae01dc7246c
David Michael Gang <michaelgang@gmail.com>
2015-12-06 16:27:19 +0200

    ex28

---
+++ src/ex21_40/ex28.py
@@ -0,0 +1,11 @@
+import time
+
+def spiral_diagonals_sum(n):
+    k = n//2
+    return round(16*(k**3)/3 + 10*k**2 +26*k/3 +1); 
+
+
+start = time.clock()
+print(spiral_diagonals_sum(1001))
+end = time.clock()
+print("It took ",end - start," seconds")
