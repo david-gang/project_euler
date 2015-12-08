b7b3ef56193fb54d09eb65dbe0e9d718d45007e9
David Michael Gang <michaelgang@gmail.com>
2015-12-08 14:58:34 +0200

    make ex26 fast

--- src/ex21_40/ex26.py
+++ src/ex21_40/ex26.py
@@ -1,41 +1,26 @@
 import time
-import itertools
-from decimal import *
-getcontext().prec = 2000
 
 """
 Stupid documentation
 """
 
 def reciprocal_cycle_length(n):
-    print(n)
-    right_side = str(Decimal(1)/Decimal(n))[2:]
-    to_check_arr = list(right_side)
-    to_check = "".join(to_check_arr)
-    max_cycle_length_all = 0
-    max_cycle_all = ''
-    for i in range(0,len(to_check) -1):
-        max_cycle_length =0
-        max_cycle = ''
-        cycle = to_check[i]
-        j = i + 1    
-        while j<len(to_check) -len(cycle):
-            if cycle == to_check[j:j+len(cycle)] and len(cycle) > max_cycle_length:
-                if cycle ==max_cycle + max_cycle:
-                    break
-                max_cycle_length = len(cycle)
-                max_cycle = cycle
-            cycle = cycle + to_check[j]
-            j = j + 1
-        if max_cycle_length > max_cycle_length_all:
-            max_cycle_length_all = max_cycle_length
-            max_cycle_all = max_cycle 
-    return max_cycle_length_all        
+    quotients = {}
+    m = 10
+    counter =0
+    while m>0:
+        if m in quotients:
+            return counter - quotients[m]  
+        quotients[m] = counter
+        m = 10*(m%n)
+        counter +=1
+    return 0
+        
     
 if __name__ == "__main__":
 	start = time.clock()
-	reciprocal_generator = ((i,Decimal(1)/Decimal(i), reciprocal_cycle_length(i)) for i in range(2,1000))
-	res = max(reciprocal_generator, key = lambda x:x[2])
+	reciprocal_generator = (reciprocal_cycle_length(i) for i in range(2,1000))
+	res = max(reciprocal_generator)
 	print(res)
 	end = time.clock()
 	print("It took ",end - start," seconds")
--- src/ex21_40/test.py
+++ src/ex21_40/test.py
@@ -4,3 +4,11 @@ from . import ex22
 class TestEx21_40(unittest.TestCase):
     def testEx22(self):
         self.assertEqual(ex22.name_scores(),871198282)
+    
+    def testEx24(self):
+        self.assertEqual(ex24.lexographic_permutation(10,10**6),2783915460)
+    
+    def testEx25(self):
+        self.assertEqual(ex24.fibonacci_order_of_magnitude(1000),4782.0)
+    
+            
